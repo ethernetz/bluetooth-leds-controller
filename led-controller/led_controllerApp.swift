e3cb4283530b795afa687c3e3e71308c952267d2
ethan netz <ethannetz@hotmail.com>
2024-01-23 12:03:57 -0500

    Initial Commit

---
+++ led-controller/led_controllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  led_controllerApp.swift
+//  led-controller
+//
+//  Created by Ethan Netz on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct led_controllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
